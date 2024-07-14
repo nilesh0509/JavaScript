4b1b1556a49def74d24640dc5d32943c6664615b
nilesh0509 <niles12105@gmail.com>
2024-07-14 23:56:42 +0530

    AC 1st practice

---
+++ ApnaCollage/1/script.js
@@ -0,0 +1,12 @@
+alert("This is ball pen , go inspect -> console . check about ball pen");
+
+const ballPan = {
+    Product : "Ball Pan",
+    Color : "Black",
+    Rating : "4/5",
+    Offer : "Deal of the day. 50% Discount",
+    Price : "Rs. 270"
+
+};
+
+console.log(ballPan);
