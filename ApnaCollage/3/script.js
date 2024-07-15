c35ad56d573e803dd19388e2e612a723ff9666a5
nilesh0509 <niles12105@gmail.com>
2024-07-15 21:36:55 +0530

    V3

---
+++ ApnaCollage/3/script.js
@@ -0,0 +1,25 @@
+// var num = prompt("Enter a number");
+
+// if(num%2 === 0){
+//     console.log("The number is even");
+// }else{
+//     console.log("The number is odd");
+// }
+
+for(let num = 0; num <= 100; num++ ){
+    if(num%2 === 0){
+        console.log("The number is even" , num);
+    }
+        
+}
+
+var num1 = 25;
+let num2 = prompt("Gusse The Number");
+while(num2!=num1){
+    num2 = prompt("Your Gauss the wrong . Try Again");
+}
+
+console.log("Congrats! YOu Gauss The Number");
+
+
+
