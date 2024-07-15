9ca9d14918758472a93be7481cc7308995966bba
nilesh0509 <niles12105@gmail.com>
2024-07-15 20:30:25 +0530

    solution 2

---
+++ ApnaCollage/2/script.js
@@ -0,0 +1,24 @@
+var num = prompt("Enter a number");
+
+if(num%5==0){
+    console.log("The number is divisible by 5");
+}else{
+    console.log("The number is not divisible by 5");
+}
+
+
+
+var Marks = prompt("Enter Your Marks");
+if(Marks>=90){
+    console.log("Grade A");
+}else if(Marks>=80){
+    console.log("Grade B");
+}else if(Marks>=70){
+    console.log("Grade C");
+}else if(Marks>=60){
+    console.log("Grade D");
+}else if(Marks>=50){
+    console.log("Grade E");
+}else{
+    console.log("Grade F");
+}
